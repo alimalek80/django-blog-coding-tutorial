127ac603c463f0e64acea36b1271988a2734477f
alimalek <alianta2016@gmail.com>
2025-05-17 15:21:23 +0300

    - modify navbar links - design the front-end of profile

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -6,6 +6,10 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['user']
+        fields = [
+            'image', 'phone', 'city', 'country',
+            'linkedin', 'github', 'website', 'x_account'
+        ]
         labels = {
             'x_account': 'X (Twitter)',
             'whatsapp': 'WhatsApp',
--- dashboard/views.py
+++ dashboard/views.py
@@ -9,7 +9,17 @@ def dashboard_home(request):
 @login_required
 def profile_view(request):
     profile = request.user.profile
-    return render(request, 'dashboard/profile/view_profile.html', {'profile': profile})
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile_view')
+    else:
+        form = ProfileForm(instance=profile)
+
+    return render(request, 'dashboard/profile/view_profile.html', {'form': form})
+
 
 @login_required
 def edit_profile(request):
